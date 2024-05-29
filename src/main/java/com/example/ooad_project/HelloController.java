c82483fa5286bc19a426c4a1713cc40a5d7beb60
Swarom Muley <oneupwallstreet@gmail.com>
2024-05-28 22:42:10 -0700

    added plants.json file

--- src/main/java/com/example/ooad_project/HelloController.java
+++ src/main/java/com/example/ooad_project/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.ooad_project;
-
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.JSONObject;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -10,5 +12,20 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+
+        // Create a JSON object
+        JSONObject jsonObject = new JSONObject();
+
+        // Add the key-value pair to the JSON object
+        jsonObject.put("album", "Led Zeppelin IV");
+
+        // Create or overwrite the JSON file
+        try (FileWriter file = new FileWriter("albums.json")) {
+            file.write(jsonObject.toString());
+            System.out.println("JSON file created successfully!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.ooad_project {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.json;
 
 
     opens com.example.ooad_project to javafx.fxml;
