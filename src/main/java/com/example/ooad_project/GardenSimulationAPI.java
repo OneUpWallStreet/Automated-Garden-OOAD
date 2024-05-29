476db878bd6ff46bacfa780f0ee7554ba4a7bc62
Swarom Muley <oneupwallstreet@gmail.com>
2024-05-28 21:41:08 -0700

    added empty GardenSimulationAPI class

---
+++ src/main/java/com/example/ooad_project/GardenSimulationAPI.java
@@ -0,0 +1,40 @@
+package com.example.ooad_project;
+
+import java.util.Map;
+
+public class GardenSimulationAPI {
+
+
+    void initializeGarden() {
+
+
+    }
+
+    Map<String, Object> getPlants(){
+        return null;
+    }
+
+    void rain(int amount) {
+
+    }
+
+
+    void temperature(int amount) {
+
+    }
+
+    void parasite(String name) {
+
+    }
+
+    void getState(){
+
+    }
+
+
+
+
+
+
+
+}
