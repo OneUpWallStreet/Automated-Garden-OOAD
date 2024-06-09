2c75a4f5b5376e3614df8b675f56725ce399fb7d
Swarom Muley <oneupwallstreet@gmail.com>
2024-06-08 18:22:06 -0700

    deleted logs

--- src/main/java/com/example/ooad_project/HelloApplication.java
+++ src/main/java/com/example/ooad_project/HelloApplication.java
@@ -27,7 +27,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
         stage.setTitle("OOAD - Project!");
-        stage.setScene(scene);
+        stage.setScene(scene) ;
         stage.show();
 
         initializeBackgroundServices();
